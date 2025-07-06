b09c4f0b030847ca85990d57dca3ebd3960eddf7
dev global <skooncode@gmail.com>
2025-07-06 18:54:09 +0530

    file added

---
+++ app2.js
@@ -0,0 +1,7 @@
+<html>
+  <body>
+    <h2>Javascript Main Page</h2>
+    <p id="main">Javascript changes</p>
+    <button type="button" onClick='document.getEmployeeById("main").innerHTML = "Hey user Welcome!"'> Submit </button>
+  </body>
+</html>
